171df14d75731adf4ffa4b9bbaef45bc49dbb48f
Hangeffekt <93764072+Hangeffekt@users.noreply.github.com>
2026-09-26 13:24:38 +0200

    Új fájlok létrehozása: index.php, main.php, menu.php és shop_connect.php; alapvető oldalbetöltési logika és hibaüzenetek kezelése

---
+++ private/shop_connect.php
@@ -0,0 +1,17 @@
+<?php
+require_once "config.php";
+try {
+  $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+function load_page_file($page) {
+    if(str_contains($page, 'main')) {
+        return __DIR__ . "\..\\" . $page . ".php";
+    }
+    return __DIR__ . "/index.php";
+}
